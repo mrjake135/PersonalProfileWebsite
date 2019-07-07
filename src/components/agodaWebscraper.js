ec81c2583229439300651cdaefe9c1cb95f19e29
Khoa Phan <khoaphan0897@gmail.com>
2019-07-07 22:27:17 +0700

    Cleaning up projects page

--- src/components/agodaWebscraper.js
+++ src/components/agodaWebscraper.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import {Grid} from 'react-mdl'
+import {Grid, Cell} from 'react-mdl'
+import sampleAgodaCities from '../images/sampleAgodaCities.png'
+import sampleAgodaHotel from '../images/sampleAgodaHotel.png'
 function agodaWebscraper() {
     return(
-        <Grid className></Grid>
+        <div style = {{width: '100%', margin:'auto'}}>
+        <Grid className = "project-learn-more">
+            <a href = "/projects" rel = "noopener noreferrer" className = "returnButton">
+            <i class="fas fa-chevron-circle-left"></i>
+            </a>
+            <Cell col={12}>
+                <div className = "bannerText">
+                    <h1>Agoda Webscraper</h1>
+                    <p>Created when working for www.gotadi.com, the company needed a webscraper to scrape through www.agoda.com. Without a relevant
+                        API, I decided to use the beautiful soup library. In the github, there are two python files, one that generates a list of hotels
+                        and the other that scrapes each individual hotel from that list of hotels. The python file that generates a list of hotels goes through
+                        agoda's list of hotels based off of a city as the chosen parameter. Thus, after knowing the hotel name and which city the hotel is from,
+                        the second file generates a link to scrape from, and then scrapes it. After scraping the webpage, the information is dumped into a json file,
+                        and moves on until all files are scraped. Below are images of that process.
+                    </p>
+                </div>
+            </Cell>
+            <Cell col = {6}><img src = {sampleAgodaCities} alt = 'sample-Agoda-Cities' className = 'sample-Agoda-Cities' ></img><p>sample city json file</p></Cell>
+            <Cell col = {6}><img src = {sampleAgodaHotel} alt = 'sample-Agoda-Hotel' className = 'sample-Agoda-Hotel' ></img><p>sample agoda hotel file</p></Cell>
+        </Grid>
+        </div>
     )
 }
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle,CardText,CardActions,Button,CardMenu,IconButton} from 'react-mdl'
-class Projects extends React.Component {
+import { Tabs, Tab, Grid, Cell, Card, CardTitle,CardText,CardActions,Button} from 'react-mdl';
+
+
+class Projects extends Component {
     constructor(props) {
         super(props)
         this.state = { activeTab: 4 };
@@ -10,7 +12,7 @@ class Projects extends React.Component {
             return (
                 <div className = "projects-grid">
                 <Card shadow={0} style={{ width: '512px', margin: 'auto' }}>
-                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>Personal Website</CardTitle>
+                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://cdn-images-1.medium.com/max/2600/1*HSisLuifMO6KbLfPOKtLow.jpeg) center / cover' }}>Personal Website</CardTitle>
                     <CardText>
                         Personal Website built using React
                     </CardText>
@@ -26,12 +28,12 @@ class Projects extends React.Component {
             return (
                 <div className = "projects-grid">
                 <Card shadow={0} style={{ width: '512px', margin: 'auto' }}>
-                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>Webscraper</CardTitle>
+                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://www.valuecoders.com/blog/wp-content/uploads/2018/05/pythonfeatured.jpg) center / cover'}}>Webscraper</CardTitle>
                     <CardText>
                         Agoda Webscraper
                     </CardText>
                     <CardActions border>
-                        <Button colored>Github</Button>
+                        <Button colored href = "https://github.com/mrjake135/WebScraper" target = "_blank" rel = "noopener noreferrer">Github</Button>
                         <Button colored href="/agodaWebscraper">Learn More</Button>
                     </CardActions>
                 </Card>
@@ -41,36 +43,38 @@ class Projects extends React.Component {
         else if (this.state.activeTab === 2) {
             return (
                 <div className = "projects-grid">
-                <Card shadow={0} style={{ width: '512px', margin: 'auto' }}>
-                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>Rock Paper Scissors</CardTitle>
-                    <CardText>
-                        Agoda Webscraper
-                    </CardText>
-                    <CardActions border>
-                        <Button colored>Github</Button>
-                        <Button colored>Learn More</Button>
-                    </CardActions>
-                </Card>
-                <Card shadow={0} style={{ width: '512px', margin: 'auto' }}>
-                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>Tetris</CardTitle>
-                    <CardText>
-                        Agoda Webscraper
-                    </CardText>
-                    <CardActions border>
-                        <Button colored>Github</Button>
-                        <Button colored>Learn More</Button>
-                    </CardActions>
-                </Card>
-                <Card shadow={0} style={{ width: '512px', margin: 'auto' }}>
-                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>RPG Game</CardTitle>
-                    <CardText>
-                        Agoda Webscraper
-                    </CardText>
-                    <CardActions border>
-                        <Button colored>Github</Button>
-                        <Button colored>Learn More</Button>
-                    </CardActions>
-                </Card>
+                    <Card shadow={0} style={{ width: '512px', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.ogivetechnology.com/images/embed_edited.jpg) center / cover' }}>Rock Paper Scissors</CardTitle>
+                        <CardText>
+                            Rock Paper Scissors implemented using C and C++  put into an embedded system
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Github</Button>
+                            <Button colored>Learn More</Button>
+                        </CardActions>
+                    </Card>
+
+                    <Card shadow={0} style={{ width: '512px', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.ogivetechnology.com/images/embed_edited.jpg) center / cover' }}>Tetris</CardTitle>
+                        <CardText>
+                            Tetris implemented using C and C++  put into an embedded system using array math
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Github</Button>
+                            <Button colored>Learn More</Button>
+                        </CardActions>
+                    </Card>
+
+                    <Card shadow={0} style={{ width: '512px', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.ogivetechnology.com/images/embed_edited.jpg) center / cover' }}>RPG Game</CardTitle>
+                        <CardText>
+                            RPG game implemented using C and C++  put into an embedded system using an implemented linked list
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Github</Button>
+                            <Button colored>Learn More</Button>
+                        </CardActions>
+                    </Card>
                 </div>
             )
         }
@@ -95,7 +99,7 @@ class Projects extends React.Component {
                     <Tab>C++</Tab>
                     <Tab>Competitions</Tab>
                 </Tabs>
-                <section className="projects-grid">
+                <section>
                     <Grid>
                         <Cell col={12}><div className="content">{this.toggleCategories()}</div></Cell>
                     </Grid>
